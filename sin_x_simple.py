88e6a8c0f78aa80c8b8449b3ebd1923134be25dd
dr_ <35027533+dayanch-r@users.noreply.github.com>
2020-08-11 14:25:27 +0300

    Update sin_x_simple.py

--- sin_x_simple.py
+++ sin_x_simple.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 from numpy import arange, sin, pi
 
 x = arange(0, 2*pi, 0.1)
-sin_x = sin(x)
 
-plt.plot(x, sin_x, label='sin(x)')
+plt.plot(x, sin(x), label='sin(x)')
 
 plt.title('A Beautiful Graph')
 plt.xlabel('x values')
